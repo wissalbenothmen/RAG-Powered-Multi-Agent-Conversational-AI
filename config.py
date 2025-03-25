9e49f4a2f5a2a1bf0c12c4b2e264cf62d8fbba80
Wissal Ben Othmen <86987426+wissalbenothmen@users.noreply.github.com>
2025-03-25 13:30:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 class Config:
     FLASK_DEBUG = True
-    GOOGLE_API_KEY = "AIzaSyA0BJ-l4g5TYK-Gd0fvK6lJMUIroDsr1rI"
+    GOOGLE_API_KEY = "api_key"
     FAISS_INDEX_PATH = str(Path("rag/index/faiss_index"))
     VECTOR_DIMENSION = 384  # Dimension for all-MiniLM-L6-v2 model
